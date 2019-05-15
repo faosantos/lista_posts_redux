dd3492e68fdea2a049b2674dc8a44d20725d204a
faosantos <faosantos@restinga.ifrs.edu.br>
2019-05-15 16:20:09 -0300

    commit

---
+++ lista_post_rudux/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxPromise from 'redux-promise';
+import reducers from './reducers';
+import Router from './Router';
+
+// higher order component: Provider
+// enhance outro component
+
+// cria application state
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Router />
+    </Provider>
+    // <View style={{ flex:1, justifyContent: 'center'}}>
+    //   <text> tessssssste</text>
+    // </View>
+  );
+};
+
+export default App;
